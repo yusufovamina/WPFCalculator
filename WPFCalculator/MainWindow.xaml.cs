955c310465b15aa7788d2219d5af756489615ead
Yusuf_hm12@itstep.edu.az <Yusuf_hm12@itstep.edu.az>
2023-10-16 11:35:51 +0400

    Add project files.

---
+++ WPFCalculator/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+
+namespace WPFCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach(UIElement item in MainGrid.Children) {
+                if (item is Button) {
+                    ((Button)item).Click += Button_CLick;
+                }
+            }
+        }
+
+        private void Button_CLick(object sender, RoutedEventArgs e)
+        {
+            string res =(string)((Button)e.OriginalSource).Content;
+          
+            if (res == "C")
+            {
+                textBlockCalc.Text = "";
+            }
+            else if(res == "=")
+                {
+                string finalCalcResult=new DataTable().Compute(textBlockCalc.Text, null).ToString();
+                textBlockCalc.Text = "";
+                textBlockCalc.Text = finalCalcResult;
+            }
+            else
+            {
+                textBlockCalc.Text += res;
+            }
+        }
+        
+    }
+}
