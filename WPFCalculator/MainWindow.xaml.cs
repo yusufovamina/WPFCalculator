6aac7e7d08759fad309c84aaf23e838d04307b0e
Amina <yusufovamina@gmail.com>
2023-10-16 20:56:43 +0400

    Commited

--- WPFCalculator/MainWindow.xaml.cs
+++ WPFCalculator/MainWindow.xaml.cs
@@ -38,17 +38,38 @@ namespace WPFCalculator
           
             if (res == "C")
             {
+                textBlockCalcEquation.Text = "";
                 textBlockCalc.Text = "";
+            }
+            else if (res=="CE")
+            { char[] arr = new char[] { '+', '-','/','*'} ;
+               int index= textBlockCalcEquation.Text.IndexOfAny(arr);
+                if (index >= 0)
+                {
+                    textBlockCalcEquation.Text = textBlockCalcEquation.Text.Substring(0, index + 1);
+                    textBlockCalc.Text = "";
+                }
+                else
+                {
+                   
+                    textBlockCalcEquation.Text = "";
+                    textBlockCalc.Text = "";
+                }
+            
             }
             else if(res == "=")
                 {
-                string finalCalcResult=new DataTable().Compute(textBlockCalc.Text, null).ToString();
+                string finalCalcResult=new DataTable().Compute(textBlockCalcEquation.Text, null).ToString();
                 textBlockCalc.Text = "";
                 textBlockCalc.Text = finalCalcResult;
             }
+            else if (res == ">")
+            {
+                textBlockCalcEquation.Text= textBlockCalcEquation.Text.Substring(0,textBlockCalcEquation.Text.Length-1);
+            }
             else
             {
-                textBlockCalc.Text += res;
+                textBlockCalcEquation.Text += res;
             }
         }
         
